7e9edc3c34f444061823774204e8d9f3874ec9f3
NabilAthillah <muhammadnabilathillah@gmail.com>
2023-05-25 05:19:02 +0700

    laract

--- backend/routes/product.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const product = require('../controllers/product.controller')
-
-router.get('/', product.findAll);
-router.post('/', product.create);
-
-module.exports = router;
