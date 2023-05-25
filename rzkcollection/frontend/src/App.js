a07051002f6db541a7f2dc35bd443bd9d40d7881
NabilAthillah <muhammadnabilathillah@gmail.com>
2023-05-25 08:17:06 +0700

    CRUD

---
+++ rzkcollection/frontend/src/App.js
@@ -0,0 +1,18 @@
+import AddProduct from "./components/AddProduct";
+import ProductList from "./components/ProductList";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import EditProduct from "./components/EditProduct";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<ProductList />}></Route>
+      <Route path="/add" element={<AddProduct />}></Route>
+      <Route path="/edit/:id" element={<EditProduct />}></Route>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
