6a1def3df5b1eb78544332e4eedc1dafb66e32a0
Dhefika <134569518+FadhlanRhmn@users.noreply.github.com>
2023-05-25 11:57:47 +0700

    frontend admin

--- rzkcollection/frontend/src/App.js
+++ rzkcollection/frontend/src/App.js
@@ -2,7 +2,10 @@ import Homepage from './pages/Homepage';
 import Shoppage from './pages/Shoppage';
 import Cartpage from './pages/Cartpage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Adminpage from './admin/pages/Homeadmin';
 import './style/style.css'
+import Myproduct from './admin/pages/Myproduct';
+import OutOfStock from './admin/pages/OutOfStock';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Route path='/' element={<Homepage />}/>
         <Route path='/shop' element={<Shoppage />}/>
         <Route path='/cart' element={<Cartpage />}/>
+        <Route path='/private/admin/admins' element={<Adminpage />}/>
+        <Route path='/private/admin/admins/myproducts' element={<Myproduct />}/>
+        <Route path='/private/admin/admins/myproducts/outofstock' element={<OutOfStock />}/>
       </Routes>
     </BrowserRouter>
   );
